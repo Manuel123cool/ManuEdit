06bbf2eb041d1ab453d2c037cf6976dcf65016cb
Manuel Kümpel <46404467+Manuel123cool@users.noreply.github.com>
2023-10-30 18:35:19 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "winFromFile.h"
+#include <iostream>
+#include <fstream>
+#include "featureNamespace.h"
+
+int main()
+{
+    char testFile[] = "testFile";
+    WinFromFile winFromFile(testFile);
+
+    return 0;
+}
